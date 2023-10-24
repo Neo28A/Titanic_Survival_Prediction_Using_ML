503225a55071af44d84a6af1dc24b0b58320baa7
Chetan M  Kittali <102372671+Neo28A@users.noreply.github.com>
2023-10-24 13:31:39 +0530

    Update titanicsurvivalprediction.py

--- TitanicSurvivalPrediction/titanicsurvivalprediction.py
+++ TitanicSurvivalPrediction/titanicsurvivalprediction.py
@@ -103,11 +103,6 @@ X.columns[X.isna().any()]
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, Y, train_size=0.25, random_state=0)
 
-#from sklearn.preprocessing import StandardScaler
-#sc = StandardScaler()
-#X_train = sc.fit_transform(X_train)
-#X_test = sc.transform(X_test)
-
 # @title *Validating some ML algorithm by its accuracy - Model Score*
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
